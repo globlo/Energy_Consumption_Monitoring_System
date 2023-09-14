fcbde7660de59f54468716c04237edb749c62c9d
globlo <49570577+globlo@users.noreply.github.com>
2023-09-13 20:12:59 -0500

    real time chart

--- energy-consumption-monitoring-system/src/App.js
+++ energy-consumption-monitoring-system/src/App.js
@@ -1,73 +1,95 @@
-
-
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Select from 'react-select';
 import Plot from 'react-plotly.js';
 
 const data = [
-  { category: 'A', value: 10 },
-  { category: 'B', value: 15 },
-  { category: 'C', value: 8 },
-  { category: 'D', value: 20 },
+  { category: 'A', values: [] },
+  { category: 'B', values: [] },
+  { category: 'C', values: [] },
+  { category: 'D', values: [] },
 ];
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedCategories, setSelectedCategories] = useState(data.map(item => item.category));
   const [filteredData, setFilteredData] = useState([]);
-  const [chartData, setChartData] = useState({});
+  const [chartData, setChartData] = useState([]);
+  const [realTimeData, setRealTimeData] = useState([...data]);
 
   useEffect(() => {
-    // Filter data based on the selected category
-    if (selectedCategory) {
-      const filtered = data.filter(item => item.category === selectedCategory.value);
-      setFilteredData(filtered);
-    } else {
-      setFilteredData([]);
-    }
-  }, [selectedCategory]);
+    // Simulate real-time updates (replace with actual data fetching logic)
+    const interval = setInterval(() => {
+      const updatedData = realTimeData.map(item => {
+        const newValue = Math.random() * 20; // Generate a new random value
+        const newValues = [...item.values, newValue]; // Add the newest value to the end
+        if (newValues.length > 10) {
+          newValues.shift(); // Remove the oldest value if there are more than 10 values
+        }
+        return { ...item, values: newValues };
+      });
+      setRealTimeData(updatedData);
+    }, 2000); // Update data every 2 seconds
+
+    return () => clearInterval(interval); // Clean up the interval on unmount
+  }, [realTimeData]);
 
   useEffect(() => {
-    // Prepare data for the bar chart
-    const categories = filteredData.map(item => item.category);
-    const values = filteredData.map(item => item.value);
+    // Filter data based on the selected categories
+    const filtered = realTimeData.filter(item => selectedCategories.includes(item.category));
+    setFilteredData(filtered);
+  }, [selectedCategories, realTimeData]);
 
-    const chartData = [
-      {
-        x: categories,
+  useEffect(() => {
+    // Prepare data for the line charts
+    const charts = selectedCategories.map(category => {
+      const filteredCategoryData = filteredData.find(item => item.category === category);
+      const values = filteredCategoryData ? filteredCategoryData.values : [];
+
+      return {
+        x: [...Array(values.length).keys()], // Create an array of indices as x values
         y: values,
-        type: 'bar',
-        marker: {
+        type: 'scatter',
+        mode: 'lines+markers',
+        name: category,
+        line: {
           color: 'blue',
         },
-      },
-    ];
+        marker: {
+          color: 'green',
+        },
+      };
+    });
 
-    setChartData(chartData);
-  }, [filteredData]);
+    setChartData(charts);
+  }, [filteredData, selectedCategories]);
 
-  const categoryOptions = [
-    { value: 'A', label: 'Category A' },
-    { value: 'B', label: 'Category B' },
-    { value: 'C', label: 'Category C' },
-    { value: 'D', label: 'Category D' },
-  ];
+  const categoryOptions = data.map(item => ({
+    value: item.category,
+    label: `Category ${item.category}`,
+  }));
 
   return (
     <div className="App">
-      <h1>Data Filtering and Plotting</h1>
+      <h1>Real-Time Data and Flowing Line Chart</h1>
       <Select
         options={categoryOptions}
-        isClearable={true}
-        onChange={option => setSelectedCategory(option)}
-        value={selectedCategory}
-        placeholder="Select a category"
+        isMulti={true}
+        onChange={options => setSelectedCategories(options.map(option => option.value))}
+        value={selectedCategories.map(category => ({
+          value: category,
+          label: `Category ${category}`,
+        }))}
+        placeholder="Select categories"
       />
       <div style={{ marginTop: '20px' }}>
         {filteredData.length > 0 ? (
           <Plot
             data={chartData}
-            layout={{ title: 'Bar Chart' }}
+            layout={{
+              title: 'Real Time Current',
+              xaxis: { title: 'Time' },
+              yaxis: { title: 'Current' },
+            }}
             config={{ responsive: true }}
           />
         ) : (
